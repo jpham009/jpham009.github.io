35b1c0750ce46844e451cede51a4ccab98f17188
Johnny Pham <jpham009@gmail.com>
2018-08-30 00:07:39 -0500

    new 2

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/*
+   
+    Template Name : Rolling - Freelancer Portfolio Template
+    Author : UiPasta Team
+    Website : http://www.uipasta.com/
+    Support : http://www.uipasta.com/support/
+	
+	
+*/
+
+
+
+/*
+   
+   Table Of Content
+   
+   1. Preloader
+   2. Smooth Scroll
+   3. Scroll Naviagation Background Change with Sticky Navigation
+   4. Mobile Navigation Hide or Collapse on Click
+   5. Scroll To Top
+   6. Typed.js
+   7. Parallax Background
+   8. Portfolio Filtering
+   9. Magnific Popup
+  10. Testimonial Carousel/Slider
+  11. Statistics Counter
+  12. Google Map
+ 
+
+*/
+
+
+(function ($) {
+     'use strict';
+       	
+        /* Typed.js */
+		
+        $(window).load(function(){
+        $(".typing").typed({
+            strings: ["Nice to meet you.", "I live in Arlington, TX.", "I love programming.", "I am a software developer."],    /* You can change the home section typing text from here and do not use "&" use "and" */
+            typeSpeed: 50,
+            backDelay: 1000,
+          });
+         });
+        
+		 
+        /* Parallax Background */
+
+        $(window).stellar({
+            responsive: true,
+            horizontalScrolling: false,
+            hideDistantElements: false,
+            horizontalOffset: 0,
+            verticalOffset: 0,
+        });
+
+   })(jQuery);
