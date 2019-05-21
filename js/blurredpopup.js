3e7cbec5c29254d6cc34958335160acf1cf5bf5e
Johnny Pham <jpham009@gmail.com>
2019-05-20 21:54:37 -0500

    update

---
+++ js/blurredpopup.js
@@ -0,0 +1,11 @@
+jQuery(function($) {'use strict';
+  
+$('#edit').on('click', function(){
+  $('body').addClass('active-modal');
+  $('.box-modal').addClass('modal-show');
+});
+
+$('.overlay').on('click', function(){
+  $('body').removeClass('active-modal');
+  $('.box-modal').removeClass('modal-show');
+})
